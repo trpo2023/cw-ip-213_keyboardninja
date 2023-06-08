ea6f874fb80c75d7f80b358fa23033c541095394
Maxim Mitkovskiy <DeadInMax@users.noreply.github.com>
2023-06-09 00:46:39 +0700

    Update and add new files files

---
+++ CMakeFiles/kbn.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kbn.dir/sources/Lastfile.c.o"
+  "CMakeFiles/kbn.dir/sources/Lastfile.c.o.d"
+  "CMakeFiles/kbn.dir/sources/main.c.o"
+  "CMakeFiles/kbn.dir/sources/main.c.o.d"
+  "kbn"
+  "kbn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kbn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
